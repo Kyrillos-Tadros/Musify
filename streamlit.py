196c4c14291104fb8fda6b1888c5ece723490872
Kyrillos <126891100+Kyrillos-Tadros@users.noreply.github.com>
2024-06-07 07:56:41 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,7 @@ from numpy_processing import load_and_process_audio
 from three_seconds_segmentation import segment_music_files
 from pytube import YouTube
 import io
+from io import BytesIO
 
 # CSS styling
 background_css = """
